379c63670b9e2368c3df4b3412fa5f80cfc84c78
Lenz Gschwendtner <lenz@springtimesoft.com>
2021-10-22 19:39:16 +0200

    first draft of a working side menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,35 @@ import topbar from "../vendor/topbar"
 import Alpine from "alpinejs"
 
 window.Alpine = Alpine;
+// Sidebar
+Alpine.store('sidebar', {
+  full: false,
+  active: "home",
+  navOpen: false
+})
+Alpine.data('dropdown', () => ({
+  open: false,
+  toggle(tab) {
+    this.open = !this.open;
+    Alpine.store('sidebar').active = tab;
+  },
+  activeClass: 'bg-gray-800 text-gray-200',
+  expandedClass: 'border-l border-gray-400 ml-4 pl-4',
+  shrinkedClass: 'sm:absolute top-0 left-20 sm:shadow-md sm:z-10 sm:bg-gray-900 sm:rounded-md sm:p-4 border-l sm:border-none border-gray-400 ml-4 pl-4 sm:ml-0 w-28'
+}))
+Alpine.data('sub_dropdown', () => ({
+  sub_open: false,
+  sub_toggle() {
+    this.sub_open = !this.sub_open;
+  },
+  sub_expandedClass: 'border-l border-gray-400 ml-4 pl-4',
+  sub_shrinkedClass: 'sm:absolute top-0 left-28 sm:shadow-md sm:z-10 sm:bg-gray-900 sm:rounded-md sm:p-4 border-l sm:border-none border-gray-400 ml-4 pl-4 sm:ml-0 w-28'
+}))
+Alpine.data('tooltip', () => ({
+  show: false,
+  visibleClass: 'block sm:absolute -top-7 sm:border border-gray-800 left-5 sm:text-sm sm:bg-gray-900 sm:px-2 sm:py-1 sm:rounded-md'
+}))
+
 Alpine.start()
 
 let Hooks = {}
@@ -63,10 +92,3 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
-// Sidebar
-document.addEventListener('alpine:init', () => {
-  Alpine.store('sidebar', {
-
-  })
-
-})
--- docs/svg.md
+++ docs/svg.md
@@ -36,3 +36,27 @@ icons are from feathericons for now
   <line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line>
   <line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line>
 </svg>
+
+### clock
+
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6">
+  <circle cx="12" cy="12" r="10"></circle>
+  <polyline points="12 6 12 12 16 14"></polyline>
+</svg>
+
+### menu
+
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6">
+  <line x1="17" y1="10" x2="3" y2="10"></line>
+  <line x1="21" y1="6" x2="3" y2="6"></line>
+  <line x1="21" y1="14" x2="3" y2="14"></line>
+  <line x1="17" y1="18" x2="3" y2="18"></line>
+</svg>
+
+
+### close
+
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6">
+  <circle cx="12" cy="12" r="10"></circle>
+  <line x1="8" y1="12" x2="16" y2="12"></line>
+</svg>
