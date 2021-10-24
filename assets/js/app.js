81f80632efc3fd59433ae2fa400883cff3f0d4e4
Lenz Gschwendtner <lenz@springtimesoft.com>
2021-10-24 17:34:04 +0200

    trying to simplify the asset pipeline

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 // We import the CSS which is extracted to its own file by esbuild.
 // Remove this line if you add a your own CSS build pipeline (e.g postcss).
-//import "../css/app.css"
+import "../css/app.css"
 
 // If you want to use Phoenix channels, run `mix help phx.gen.channel`
 // to get started and then uncomment the line below.
@@ -26,6 +26,8 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 import Alpine from "alpinejs"
+
+require("typeface-quicksand");
 
 window.Alpine = Alpine;
 // Sidebar
--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,11 +2,15 @@ module.exports = {
   mode: "jit",
   purge: [
     "./js/**/*.js", 
-    "../lib/*_web/**/*.*ex"
+    "../lib/*_web/**/*.*ex",
+    "../../lib/*_web/**/*.*ex"
   ],
   darkMode: false, // false or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Quicksand", "sans-serif"]
+      }
     },
   },
   variants: {},
